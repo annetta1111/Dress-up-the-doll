2468f6c1c07f2b68ff13bb45cda8a43f8fc88e62
annetta1111 <cool.xarens@yandex.ru>
2026-05-31 17:15:35 +0300

    Add files via upload
    База данных всей одежды. Содержит словарь CLOTHES с 96 предметами: платья, верх, обувь, волосы, аксессуары, низ.

---
+++ clothes_data.py
@@ -0,0 +1,60 @@
+CLOTHES = {
+
+"👗": [
+    {
+        "name": f"dress{i}",
+        "image": f"images/clothes/dresses/dress{i}.png",
+        "cat": "dress"
+    }
+    for i in range(1, 14)
+],
+
+"👚": [
+    {
+        "name": f"top{i}",
+        "image": f"images/clothes/tops/top{i}.png",
+        "cat": "top"
+    }
+    for i in range(1, 28)
+],
+
+
+"👠": [
+    {
+        "name": f"shoes{i}",
+        "image": f"images/clothes/shoes/shoes{i}.png",
+        "cat": "shoes"
+    }
+    for i in range(1, 12)
+],
+
+
+"💇": [
+    {
+        "name": f"hair{i}",
+        "image": f"images/clothes/hair/hair{i}.png",
+        "cat": "hair"
+    }
+    for i in range(1, 11)
+],
+
+
+"🎀": [
+    {
+        "name": f"accessory{i}",
+        "image": f"images/clothes/accessories/accessory{i}.png",
+        "cat": "accessory"
+    }
+    for i in range(1, 17)
+],
+
+"👖": [
+    {
+        "name": f"bottom{i}",
+        "image": f"images/clothes/bottoms/bottom{i}.png",
+        "cat": "bottom"
+    }
+    for i in range(1, 19)
+]
+
+}
